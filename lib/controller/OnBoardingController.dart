ea5a2eb2ded3be88bb9e33336a79454f25f48041
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-03-31 14:23:38 +0300

    final onBoarding page add button nextSlide

--- lib/controller/OnBoardingController.dart
+++ lib/controller/OnBoardingController.dart
@@ -2,6 +2,8 @@
 // ignore: file_names
 // ignore_for_file: file_names
 
+import 'package:ecommercesystem/data/datasource/static/Static.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
 abstract class OnBoardingController extends GetxController {
@@ -10,15 +12,28 @@ abstract class OnBoardingController extends GetxController {
 }
 
 class OnBoardingControllerImp extends OnBoardingController {
+  late PageController pageController;
   int currntPage = 0;
   @override
   next() {
-    // throw UnimplementedError();
+    if (currntPage > onBoardingList.length - 2) {
+    } else {
+      currntPage++;
+      pageController.animateToPage(currntPage,
+          duration: const Duration(milliseconds: 300), curve: Curves.linear);
+    }
+    update();
   }
 
   @override
   onChangeSlider(index) {
     currntPage = index;
     update();
+  }
+
+  @override
+  void onInit() {
+    pageController = PageController();
+    super.onInit();
   }
 }
--- lib/view/widget/onBoarding.dart/ButtonNext.dart
+++ lib/view/widget/onBoarding.dart/ButtonNext.dart
@@ -2,10 +2,12 @@
 // ignore: file_names
 // ignore_for_file: file_names
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:ecommercesystem/controller/OnBoardingController.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import '../../../core/constant/ScreenSize.dart';
 
-class ButtonNext extends StatelessWidget {
+class ButtonNext extends GetView<OnBoardingControllerImp> {
   const ButtonNext({super.key});
 
   @override
@@ -14,7 +16,9 @@ class ButtonNext extends StatelessWidget {
       width: ScreenSize.widthScreen(context) / 2,
       height: ScreenSize.heightScreen(context) / 15,
       child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            controller.next();
+          },
           child: const AutoSizeText(
             'Let\'s got',
             maxLines: 1,
